e21ebcb9e82dc2e5ba18a16ec22eb8a01c7c7620
Guillaume Buret <guiguibu91@gmail.com>
2023-12-03 23:16:38 +0100

    Utils class

---
+++ process-live-data/src/Utils.py
@@ -0,0 +1,26 @@
+import numpy as np
+from shapely.geometry import LineString
+from shapely.ops import unary_union
+from pyproj import Geod
+
+class Utils:
+
+    @staticmethod
+    def get_linestring_length_in_meters(line):
+        # Distance in meter
+        geod = Geod(ellps="WGS84")
+        return geod.geometry_length(line)
+
+    @staticmethod
+    def interpolate_linestring(line, distance_between_points=None, n=None):
+        if distance_between_points and distance_between_points > 0:
+            n = round(Utils.get_linestring_length_in_meters(line) / distance_between_points)
+        elif n and n > 1:
+            n = n
+        else:
+            n = 10
+        
+        distances = np.linspace(0, line.length, n)
+        points = [line.interpolate(distance) for distance in distances]
+        interpolated_line = LineString(points)
+        return interpolated_line
