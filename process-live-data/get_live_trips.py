2fc63519bf7b86fa26f9c076343ab58e7c7f933d
Guillaume Buret <guiguibu91@gmail.com>
2023-12-13 20:39:27 +0100

    get live trips

--- process-live-data/get_live_trips.py
+++ process-live-data/get_live_trips.py
@@ -13,18 +13,18 @@ from shapely.ops import nearest_points, linemerge
 from itertools import combinations
 import random
 
-import aiohttp
-from aiolimiter import AsyncLimiter
 import asyncio
 import time
 import datetime
+import aiohttp
+from aiolimiter import AsyncLimiter
 
 from src.PRIM_API import PRIM_API
 from src.ArrivalTime import ArrivalTime
 
 start_time = time.time()
 
-# Limit to X requests/second
+# Limit to 50 requests/second
 limiter = AsyncLimiter(50, time_period=1)
 
 # Load settings from settings.json
@@ -40,6 +40,7 @@ stops = gpd.read_file('data/stops.json')
 
 print("Load network and stops: %s seconds" % (time.time() - start_time))
 
+
 def get_remaining_time_until_next_fetch():
     # Get the current time
     now = datetime.datetime.now()
@@ -53,15 +54,57 @@ def get_remaining_time_until_next_fetch():
         fetch_frequency = 3
     elif h >= 0 and h < 2:
         fetch_frequency = 4
-    elif h >=7 and h <= 22:
+    elif h >= 7 and h <= 22:
         fetch_frequency = 2
     elif h == 23:
         fetch_frequency = 3
     else:
         fetch_frequency = 15
-    
+
     remaining_time = 60 * (fetch_frequency - m % fetch_frequency)
     return remaining_time
+
+
+def parse_trip_json(trip, stop_id):
+
+    # Get train name (if SNCF)
+    name = ""
+    try:
+        name = trip['MonitoredVehicleJourney']['TrainNumbers']['TrainNumberRef'][0]['value']
+    except:
+        pass
+
+    # Get trip attributes
+    try:
+        if 'ExpectedArrivalTime' in trip['MonitoredVehicleJourney']['MonitoredCall'].keys():
+            arrival_time = trip['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
+            arrival_time = ArrivalTime.parse_date_from_string(arrival_time)
+            arrival_time = ArrivalTime(arrival_time)
+        else:
+            return None
+
+        id = trip['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
+
+        line_id = trip['MonitoredVehicleJourney']['LineRef']['value']
+        line_id = line_id.rstrip(":").split(":")[-1]
+
+        destination_id = trip['MonitoredVehicleJourney']['DestinationRef']['value']
+        stop_name = stops[stops.short_id==stop_id].iloc[0]['name']
+
+        trip_dict = {'id': id,
+                     'stop_id': stop_id,
+                     'stop_name': stop_name,
+                     'name': name,
+                     'line_id': line_id,
+                     'destination_id': destination_id,
+                     'arrival_time': arrival_time
+                     }
+
+        return trip_dict
+    except Exception as e:
+        print(e)
+        print(trip)
+        return None
 
 
 async def get_next_trains_from_stop(stop_id, url, session, prim_api):
@@ -72,61 +115,61 @@ async def get_next_trains_from_stop(stop_id, url, session, prim_api):
 
             try:
                 trips = json_data['Siri']['ServiceDelivery']['StopMonitoringDelivery'][0]['MonitoredStopVisit']
-            except:
+            except Exception as e:
+                print(e)
                 print(json_data)
-                
 
-            def parse_json_data(trip):
-                # Get train name (if SNCF)
-                try:
-                    name = trip['MonitoredVehicleJourney']['TrainNumbers']['TrainNumberRef'][0]['value']
-                except:
-                    name = ""
+            # Only return the next trip
+            # return [parse_trip_json(trips[0], stop_id)]
 
-                # Get trip attributes
-                try:
-                    arrival_time = trip['MonitoredVehicleJourney']['MonitoredCall']['ExpectedArrivalTime']
-                    arrival_time = ArrivalTime.parse_date_from_string(arrival_time)
-                    arrival_time = ArrivalTime(arrival_time)
+            return [parse_trip_json(trip, stop_id) for trip in trips]
 
-                    id = trip['MonitoredVehicleJourney']['FramedVehicleJourneyRef']['DatedVehicleJourneyRef']
-                    line_id = trip['MonitoredVehicleJourney']['LineRef']['value']
-                    line_id = line_id.rstrip(":").split(":")[-1]
-
-                    return {'id': id, 'stop_id': stop_id, 'name': name, 'line_id': line_id, 'arrival_time': arrival_time}
-                except:
-                    print(trip)
-                    return None
-
-            return [parse_json_data(trip) for trip in trips]
 
 def build_time_path(df, line_id):
     shortest_paths = gpd.read_file(f'data/shortest_paths/{line_id}.gpkg')
 
 
-async def fetch_arrival_times(line_id):
+def rebuild_trips(trips):
+    return trips
+
+
+async def get_trips(line_id):
     tasks = []
-    short_ids = list(set(stops[stops['line_id']==line_id].short_id.values))
+    short_ids = list(set(stops[stops['line_id'] == line_id].short_id.values))
+    transportation_type = network[network.id==line_id].iloc[0].transportation_type
 
     async with aiohttp.ClientSession() as session:
+        # Fetch arrival times at each stop
         for short_id in short_ids:
-            url = prim.NEXT_TRIPS_BASE_URL + urllib.parse.quote(f"STIF:StopPoint:Q:{short_id}:")
-            tasks.append(asyncio.ensure_future(get_next_trains_from_stop(short_id, url, session, prim)))
+            url = prim.NEXT_TRIPS_BASE_URL + \
+                urllib.parse.quote(f"STIF:StopPoint:Q:{short_id}:")
+            tasks.append(asyncio.ensure_future(
+                get_next_trains_from_stop(short_id, url, session, prim)))
         print("Created async tasks: %s seconds" % (time.time() - start_time))
 
         responses = await asyncio.gather(*tasks)
         print("Execute taks: %s seconds" % (time.time() - start_time))
 
-        next_trains = [t for r in responses for t in r if t is not None]
-        next_trains = pd.DataFrame.from_dict(next_trains)
+        # print(responses)
 
-        next_trains['time_position'] = list(zip(next_trains.stop_id, next_trains.arrival_time))
-        next_trains = next_trains.groupby(['id', 'line_id'])['time_position'].agg(['unique'])
+        # Generate trips dataframe for the line
+        trips = [t for r in responses for t in r if t is not None]
+        trips = pd.DataFrame.from_dict(trips)
 
-        
+        # Link time and positions for each trip
+        trips['time_position'] = list(zip(trips.stop_id, trips.arrival_time, trips.stop_name))
 
-        print("Generate dataframe from response: %s seconds" % (time.time() - start_time))
-        return next_trains
+        # RATP data is not complete for metro and tramway
+        # Thus we have to manually build trips using the timetable and real-time data for next trains.
+        if transportation_type in ("TRAMWAY", "METRO"):
+            rebuild_trips(trips)
+        trips_groupby = trips.groupby(['id', 'line_id', 'destination_id'])['time_position'].unique().reset_index()
+
+        # Load shortest paths database
+        shortest_paths = gpd.read_file(f'data/shortest_paths/{line_id}.gpkg')
+
+        print(f"Generate dataframe from response: {time.time() - start_time} seconds")
+        return trips, trips_groupby
 
 
 async def main():
@@ -135,7 +178,7 @@ async def main():
         line_ids = list(set(stops.line_id))[:5]
 
         arrival_times = await asyncio.gather(
-            *[fetch_arrival_times(line_id) for line_id in line_ids]
+            *[get_trips(line_id) for line_id in line_ids]
         )
 
         time_to_sleep = get_remaining_time_until_next_fetch()
@@ -144,4 +187,9 @@ async def main():
 
 
 # Run the main coroutine
-thread = asyncio.run(main())
+#thread = asyncio.run(main())
+
+# Test for one line
+line_id = "C01383"
+shortest_paths = gpd.read_file(f'data/shortest_paths/{line_id}.gpkg')
+t, t2 = asyncio.run(get_trips(line_id))
