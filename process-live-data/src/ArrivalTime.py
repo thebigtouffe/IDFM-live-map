cafecf4bf271c14c6352529ab6010d7649cdea0b
Guillaume Buret <guiguibu91@gmail.com>
2024-03-05 20:29:15 +0100

    parse_time_from_string in ArrivalTime

--- process-live-data/src/ArrivalTime.py
+++ process-live-data/src/ArrivalTime.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import pytz
 
 class ArrivalTime:
     def __init__(self, time):
@@ -8,6 +9,10 @@ class ArrivalTime:
     @staticmethod
     def parse_date_from_string(s):
         return datetime.strptime(s, "%Y-%m-%dT%H:%M:%S.%fZ")
+
+    @staticmethod
+    def parse_time_from_string(s):
+        return datetime.strptime(s, "H:%M:%S")
 
     def __repr__(self):
         return str(self.time)
